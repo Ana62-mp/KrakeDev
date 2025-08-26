27d36f3be4136f9d34c7ea3c823c3b38efe58f6a
Ana62-mp <ana.moposita84@gmail.com>
2025-08-25 21:21:53 -0500

    boton cambia de gif

--- ejercicioGit/ejercicioGit.js
+++ ejercicioGit/ejercicioGit.js
@@ -1,3 +1,7 @@
 saludar = function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById('imgSaludo');
+    cmpImagenSaludo.src = './imagenes/gif2.gif'
     console.log('Ingresa a Saludar');
 }
+
