de1ab913fca5ca9b4cb9fd5806d6a58e27429896
Ana62-mp <ana.moposita84@gmail.com>
2025-08-26 19:24:28 -0500

    se agrego funcion recuperarInt/Float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,11 +1,24 @@
 saludar = function(){
     let nombre = recuperarTexto('txtNombre');
     let apellido = recuperarTexto('txtApellido');
+    let edad = recuperarInt('txtEdad');
+    let estatura = recuperarFloat('txtEstatura');
 }
+
 recuperarTexto = function(idComponente){
     let componente = document.getElementById(idComponente);
     let valorIngresado = componente.value;
     return valorIngresado;
 }
 
+recuperarInt = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente)
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero
+}
 
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente)
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante
+}
