6d3dfb5160b7611be425922b6c7b2c0cc13aca75
Ana62-mp <ana.moposita84@gmail.com>
2025-09-05 23:48:30 -0500

    Info placas ver.2

--- placas/placas.js
+++ placas/placas.js
@@ -1,12 +1,45 @@
 validarPlacas = function(){
-    let erroresEstructura;
-    let cmpTexto = recuperarTexto('txtPlaca');
-    erroresEstructura = validarEstructura(cmpTexto);
+    let cmpPlaca = recuperarTexto('txtPlaca');
+    let erroresEstructura = validarEstructura(cmpPlaca);
+    let provinciaPlaca = obtenerProvincia(cmpPlaca);
+    let tipoDeVehiculo = obtenerTipoVehiculo(cmpPlaca);
+
     if(erroresEstructura == null){
         mostrarTexto('lblResultado', 'ESTRUCTURA VALIDA');
         mostrarTexto('lblError1', '');
+
+        if(provinciaPlaca != null){
+            mostrarTexto('lblProvincia', provinciaPlaca);
+        }else{
+            mostrarTexto('lblProvincia', 'PROVINCIA INCORRECTA');
+        }
+
+        if(tipoDeVehiculo!= null){
+            mostrarTexto('lblVehiculo', tipoDeVehiculo);
+        }else{
+            mostrarTexto('lblVehiculo', 'VEHICULO INCORRECTA');
+        }
+        
+        let diaPicoYPlaca = obtenerDiaPicoYPlaca(cmpPlaca);
+        mostrarTexto('lblPicoPlaca', diaPicoYPlaca + '\n\nRecuerda que Sábados, Domingos y Feriados\nla circulación es libre.');
+
+        
     }else{
         mostrarTexto('lblResultado', 'ESTRUCTURA INCORRECTA');
         mostrarTexto('lblError1', erroresEstructura);
+        mostrarTexto('lblProvincia', 'ESCRIBE CORRECTAMENTE LA\nESTRUCTURA PARA CONOCER LA PROVINCIA');
+        mostrarTexto('lblVehiculo', 'ESCRIBE CORRECTAMENTE LA\nESTRUCTURA PARA CONOCER EL VEHICULO');
+        mostrarTexto('lblPicoPlaca', 'ESCRIBE CORRECTAMENTE LA\nESTRUCTURA PARA CONOCER EL DIA');
     }
+}
+
+limpiar = function () {
+    mostrarTextoEnCaja('txtPlaca', '');
+
+    mostrarTexto('lblResultado', '...');
+    mostrarTexto('lblError1', '');
+    mostrarTexto('lblProvincia', '...');
+    mostrarTexto('lblVehiculo', '...');
+    mostrarTexto('lblPicoPlaca', '...');
+
 }
--- placas/serviciosPlacas.js
+++ placas/serviciosPlacas.js
@@ -47,3 +47,164 @@ validarEstructura = function(placa){
        
     
 }
+
+obtenerProvincia = function(placa){
+    let primeraLetra = placa.charAt(0);
+    let provincia = '';
+
+    if(primeraLetra == 'A'){
+        provincia = 'Azuay';
+    }
+        
+    if(primeraLetra == 'B'){
+        provincia = 'Bolívar';
+    }
+        
+    if(primeraLetra == 'U'){
+        provincia = 'Cañar';
+    }
+        
+    if(primeraLetra == 'C'){
+        provincia = 'Carchi';
+    }
+        
+    if(primeraLetra == 'X'){
+        provincia = 'Cotopaxi';
+    }
+        
+    if(primeraLetra == 'H'){
+        provincia = 'Chimborazo';
+    }
+        
+    if(primeraLetra == 'O'){
+        provincia = 'El Oro';
+    }
+        
+    if(primeraLetra == 'E'){
+        provincia = 'Esmeraldas';
+    }
+        
+    if(primeraLetra == 'W'){
+        provincia = 'Galápagos';
+    }
+        
+    if(primeraLetra == 'G'){
+        provincia = 'Guayas';
+    }
+        
+    if(primeraLetra == 'I'){
+        provincia = 'Imbabura';
+    }
+        
+    if(primeraLetra == 'L'){
+        provincia = 'Loja';
+    }
+        
+    if(primeraLetra == 'R'){
+        provincia = 'Los Ríos';
+    }
+        
+    if(primeraLetra == 'M'){
+        provincia = 'Manabí';
+    }
+        
+    if(primeraLetra == 'V'){
+        provincia = 'Morona Santiago';
+    }
+        
+    if(primeraLetra == 'N'){
+        provincia = 'Napo';
+    }
+        
+    if(primeraLetra == 'S'){
+        provincia = 'Pastaza';
+    }
+        
+    if(primeraLetra == 'P'){
+        provincia = 'Pichincha';
+    }
+        
+    if(primeraLetra == 'K'){
+        provincia = 'Sucumbíos';
+    }
+        
+    if(primeraLetra == 'Q'){
+        provincia = 'Orellana';
+    }
+        
+    if(primeraLetra == 'T'){
+        provincia = 'Tungurahua';
+    }
+        
+    if(primeraLetra == 'Z'){
+        provincia = 'Zamora Chinchipe';
+    }
+        
+    if(primeraLetra == 'Y'){
+        provincia = 'Santa Elena';
+    }
+    
+    if(provincia == ''){
+        return null;
+    }else{
+        return provincia;
+    }
+}
+
+obtenerTipoVehiculo = function(placa){
+    let segundaLetra = placa.charAt(1);
+    let tipoVehiculo = '';
+
+    if(segundaLetra == 'A' || segundaLetra == 'Z'){
+        tipoVehiculo = 'Vehículos comerciales\n(como taxis o autobuses)';
+    }
+        
+    if(segundaLetra == 'E'){
+        tipoVehiculo = 'Vehículos gubernamentales';
+    }
+        
+    if(segundaLetra == 'X'){
+        tipoVehiculo = 'Vehículos de uso oficial';
+    }
+        
+    if(segundaLetra == 'S'){
+        tipoVehiculo = 'Vehículos del gobierno\nprovincial';
+    }
+        
+    if(segundaLetra == 'M'){
+        tipoVehiculo = 'vehículos municipales';
+    }
+        
+    if(segundaLetra != 'A' && segundaLetra != 'Z' && segundaLetra != 'E' && segundaLetra != 'X' && segundaLetra != 'S' && segundaLetra != 'M'){
+        tipoVehiculo = 'Vehículos particular\n(privado)';
+    }
+    
+    if(tipoVehiculo == ''){
+        return null;
+    }else{
+        return tipoVehiculo;
+    }
+}
+
+obtenerDiaPicoYPlaca = function(placa){
+    let ultimaPosicion = placa.length - 1;
+    let ultimoDigito = placa.charAt(ultimaPosicion);
+    let diaPicoPlaca;
+
+    if(ultimoDigito == 1 || ultimoDigito == 2){
+        diaPicoPlaca = 'Lunes';
+    }
+    if(ultimoDigito == 3 || ultimoDigito == 4){
+        diaPicoPlaca = 'Martes';
+    }
+    if(ultimoDigito == 5 || ultimoDigito == 6){
+        diaPicoPlaca = 'Miércoles';
+    }
+    if(ultimoDigito == 7 || ultimoDigito == 8){
+        diaPicoPlaca = 'Jueves';
+    }
+    if(ultimoDigito == 9 || ultimoDigito == 0){
+        diaPicoPlaca = 'Viernes';
+    }
+    return diaPicoPlaca;
+}
