a7f008962e8af2e21fb35047d39155461d1a4408
Ana62-mp <ana.moposita84@gmail.com>
2025-09-08 17:34:03 -0500

    ejercicios bucle FOR

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,56 @@
+mostrarNumeros = function(){
+    console.log('ANTES DEL FOR');
+
+    for(let i=0;i<4;i++){
+        console.log(i);
+    }
+
+    console.log('DESPUÉS DEL FOR')
+}
+
+mostrarNumeros2 = function(){
+    console.log('ANTES DEL FOR');
+
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+
+    console.log('DESPUÉS DEL FOR')
+}
+
+mostrarPares = function(){
+    console.log('ANTES DEL FOR');
+
+    for(let x=2;x<=10;x+=2){
+        console.log(x);
+    }
+
+    console.log('DESPUÉS DEL FOR')
+}
+
+mostrarInversa = function(){
+    console.log('ANTES DEL FOR');
+
+    for(let i=10;i>0;i--){
+        console.log(i);
+    }
+
+    console.log('DESPUÉS DEL FOR')
+}
+
+hackearNasaPelis = function(){
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log('hackeando Nasa '+porcentaje+'%');
+    }
+    console.log('LA NASA HA SIDO HACKEADA');
+}
+
+mostrarImpares = function(){
+    console.log('ANTES DEL FOR');
+
+    for(let x=1;x<=21;x+=2){
+        console.log(x);
+    }
+
+    console.log('DESPUÉS DEL FOR')
+}
