1292278b6cf4f3469caccb618620675d66370fa7
Ana62-mp <ana.moposita84@gmail.com>
2025-09-12 21:41:49 -0500

    juego ahorcado paso 0

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,1 +1,9 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter){
+    codigoCaracter = caracter.charCodeAt(0);
+    let esMayus = true;
+    if(codigoCaracter < 65 || codigoCaracter > 90){
+        esMayus = false;
+    }
+    return esMayus;
+}
