42dd19193b5b409f4f27a38339303295b424333e
Ana62-mp <ana.moposita84@gmail.com>
2025-09-13 20:07:18 -0500

    juego ahorcado paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,9 +1,34 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+
+let palabraSecreta;
+
 esMayuscula = function(caracter){
-    codigoCaracter = caracter.charCodeAt(0);
     let esMayus = true;
+    let codigoCaracter = caracter.charCodeAt(0);
     if(codigoCaracter < 65 || codigoCaracter > 90){
         esMayus = false;
     }
     return esMayus;
 }
+
+guardarPalabra = function(){
+    let palabra = recuperarTexto('txtSecreta');
+    let caracterIterado;
+    let esMayus = false;
+
+    for(let i=0;i<5;i++){
+            caracterIterado = palabra.charAt(i);
+            if(esMayuscula(caracterIterado)){
+                esMayus = true
+            }
+        }
+
+    if(palabra.length == 5 && esMayus){
+        palabraSecreta = palabra;
+        console.log(palabraSecreta);
+        mostrarTexto('txtError', '')
+    }else{
+        mostrarTexto('txtError', 'Ingresa palabra de 5 letras mayúsculas')
+    }
+}
+
