87845b2d1f79f188e15b0d44e5790074ff4f0373
Ana62-mp <ana.moposita84@gmail.com>
2025-09-22 20:47:05 -0500

    actualización objetos y clientes

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -43,3 +43,55 @@ crearProducto = function(){
 
 }
 
+
+modificarAtributos = function(){
+    let cuenta = {
+        numero : '218931983',
+        saldo : 0.0,
+
+    }
+    cuenta.saldo = 100;
+    cuenta.saldo += 10;
+    console.log(cuenta.saldo);
+}
+
+crearCliente = function(){
+    let cliente = {
+        cedula : '1754742728',
+        nombre : 'Ana'
+    }
+    let cliente1 = {};
+    cliente1.nombre = 'Romeo';
+    cliente1.apellido = 'Salcedo';
+    cliente1.cedula = '123123';
+}
+
+probarIncrementarSaldo = function(){
+    let cta = {numero:'23245', saldo:34.0};
+    incrementarSaldo(cta, 100);
+    console.log(cta.saldo);
+}
+
+probarDeterminarMayor = function(){
+    let persona1 = {nombre:'Ana',edad:45};
+    let persona2 = {nombre:'Maria',edad:48};
+    let mayor = determinarMayor(persona1, persona2);
+    if(mayor !=null){
+        console.log('El mayor es: ' + mayor.nombre);
+    }
+}
+
+incrementarSaldo = function(cuenta, monto){
+    cuenta.saldo += monto;
+
+}
+
+determinarMayor = function(persona1, persona2){
+    if(persona1.edad > persona2.edad){
+        return persona1;
+    }else if(persona1.edad < persona2.edad){
+        return persona2;
+    }else{
+        return null;
+    }
+}
