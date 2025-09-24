95f0dfcfcf8aa8d9f06b25ba4b35ab3bbdcdd78d
Ana62-mp <ana.moposita84@gmail.com>
2025-09-23 21:43:07 -0500

    roles parte 2

--- roles/roles.js
+++ roles/roles.js
@@ -1,13 +1,40 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1752724747",nombre:"Laura",apellido:"Lazo",sueldo:750.0}
 ]
+
+mostrarEmpleado = function(){
+    let cmpTabla = document.getElementById('tablaEmpleados');
+    let contenidoTabla = '<table><tr>'+
+    '<th>CEDULA</th>'+
+    '<th>NOMBRE</th>'+
+    '<th>APELLIDO</th>'+
+    '<th>SUELDO</th>'+
+    '</tr>';
+
+
+    let elementoEmpleado;
+    for(let i=0;i<empleados.length; i++){
+        elementoEmpleado = empleados[i];
+        contenidoTabla+= '<tr><td>'+elementoEmpleado.cedula+'</td>'
+        + '<td>'+elementoEmpleado.nombre+'</td>'
+        + '<td>'+elementoEmpleado.apellido+'</td>'
+        + '<td>'+elementoEmpleado.sueldo+'</td>'
+        + '</tr>'
+    }
+
+    contenidoTabla += '</table>';
+
+    cmpTabla.innerHTML = contenidoTabla;
+}
 
 
 mostrarOpcionEmpleado = function(){
     mostrarComponente('divEmpleado')
     ocultarComponente('divRol')
     ocultarComponente('divResumen')
+    mostrarEmpleado();
 }
 mostrarOpcionRol = function(){
     mostrarComponente('divRol')
